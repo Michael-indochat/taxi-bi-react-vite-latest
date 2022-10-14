82c5579bb89fd61828b3250f789f5362e5f1ba3e
Michael Luo <michael@indochat.co.id>
2022-10-14 14:32:55 +0800

    update: 刪除多餘的 div

--- src/components/InnerContent.jsx
+++ src/components/InnerContent.jsx
@@ -2,12 +2,9 @@ import { Outlet } from "react-router-dom";
 
 const InnerContent = () => {
   return (
-    <div className="inner-content text-lg font-bold text-white w-full">
-      {/* container */}
-      <main>
+      <>
         <Outlet />
-      </main>
-    </div>
+      </>
   );
 };
 
